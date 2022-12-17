90abb78d25ed8415a0588672e8df621342f009f8
Thong Phan <phtrithong@gmail.com>
2022-12-17 22:54:40 +0700

    [feat] init and add first cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, MouseEventHandler } from "react";
+import { motion, Variants } from "framer-motion";
+import "./App.css";
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+  const cursorVariants: Variants = {
+    default: {
+      left: mousePosition.x - 48,
+      top: mousePosition.y - 32,
+    },
+    text: {
+      left: mousePosition.x - 75,
+      top: mousePosition.y - 75,
+      height: 150,
+      width: 150,
+      backgroundColor: "yellow",
+      mixBlendMode: "difference",
+      transition: {
+        type: "spring",
+      },
+    },
+  };
+
+  const textVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      marginLeft: -25,
+    },
+    show: {
+      opacity: 1,
+      marginLeft: 0,
+      transition: {
+        type: "spring",
+      },
+    },
+  };
+
+  const titleVariants: Variants = {
+    hidden: {
+      opacity: 0,
+    },
+    show: (i = 1) => ({
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.15,
+        delayChildren: i * 0.15,
+      },
+    }),
+  };
+
+  const handleMouseEnter: MouseEventHandler = () => {
+    setCursorVariant("text");
+  };
+  const handleMouseLeave: MouseEventHandler = (e) => {
+    setCursorVariant("default");
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setMousePosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const renderAnimatedText = (text: string) => {
+    return Array.from(text).map((letter, index) => (
+      <motion.span variants={textVariants} key={index} className="character">
+        {letter}
+      </motion.span>
+    ));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <section className="section">
+        <motion.h1
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="title"
+          variants={titleVariants}
+          initial="hidden"
+          whileInView="show"
         >
-          Learn React
-        </a>
-      </header>
+          {renderAnimatedText("Thong Phan")}
+        </motion.h1>
+        <motion.div
+          className="cursor"
+          variants={cursorVariants}
+          animate={cursorVariant}
+          transition={{ ease: "easeOut", duration: 0.1 }}
+        />
+      </section>
     </div>
   );
 }
